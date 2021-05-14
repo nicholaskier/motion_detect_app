e7948e1bc3417fa8c676c2b498d0a97e2f3e0d47
unknown <nicholaskier@gmail.com>
2021-05-14 13:01:16 -0500

    dilate white space

--- motion_detector.py
+++ motion_detector.py
@@ -17,9 +17,14 @@ while True:
     
     delta_frame = cv2.absdiff(first_frame, gray)
 
-    
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+
+
     cv2.imshow("Capturing", gray)
     cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold frame", thresh_frame)
 
     key = cv2.waitKey(1)
     print(gray)
