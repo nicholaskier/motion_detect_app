f94180b2c7a52217a35804a432e2f2cf21851052
unknown <nicholaskier@gmail.com>
2021-05-14 13:08:34 -0500

    add tracking rectangles

--- motion_detector.py
+++ motion_detector.py
@@ -20,11 +20,18 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x + w, y + h), (0,255,0), 3)
 
     cv2.imshow("Capturing", gray)
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("Threshold frame", thresh_frame)
+    cv2.imshow("color frame", frame)
+
 
     key = cv2.waitKey(1)
     print(gray)
