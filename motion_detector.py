daa41f53175f4055a50d2feb7a8198359ac2cf28
unknown <nicholaskier@gmail.com>
2021-05-14 13:33:48 -0500

    add time capture

--- motion_detector.py
+++ motion_detector.py
@@ -1,12 +1,17 @@
 import cv2, time
+from datetime import datetime
 
 first_frame = None
+status_list = [None, None]
+times = []
 
 video = cv2.VideoCapture(0)
 
 
 while True:
     check, frame = video.read()
+
+    status = 0
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21,21),0)
@@ -22,10 +27,19 @@ while True:
 
     (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x + w, y + h), (0,255,0), 3)
+
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[0] == 1 and status_list[1] == 0:
+        times.append(datetime.now())
+
+    
 
     cv2.imshow("Capturing", gray)
     cv2.imshow("Delta Frame", delta_frame)
@@ -34,10 +48,10 @@ while True:
 
 
     key = cv2.waitKey(1)
-    print(gray)
 
     if key == ord('q'):
         break
+    print(status)
 
 video.release()
 cv2.destroyAllWindows
